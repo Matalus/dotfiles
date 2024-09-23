0754fd3e10e9adba33adbbbb7771bb77273574a0
Matt Hamende <matt.hamende@gmail.com>
2024-09-23 11:57:24 -0700

    Refactoring Telescope Configuration

--- .nvim/lua/plugins/example.lua
+++ .nvim/lua/plugins/example.lua
@@ -44,27 +44,35 @@ return {
   },
 
   -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim"
+  --   },
+  --   keys = {
+  --     -- add a keymap to browse plugin files
+  --     -- stylua: ignore
+  --     {
+  --       "<leader>fp",
+  --       function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+  --       desc = "Find Plugin File",
+  --     },
+  --     {
+  --       "<leader>gS",
+  --       function() require("telescope.builtin").git_branches() end,
+  --       desc = "Find Git Branches",
+  --     }
+  --   },
+  --   -- change some options
+  --   opts = {
+  --     defaults = {
+  --       layout_strategy = "horizontal",
+  --       layout_config = { prompt_position = "top" },
+  --       sorting_strategy = "ascending",
+  --       winblend = 0,
+  --     },
+  --   },
+  -- },
 
 
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
--- .nvim/lua/plugins/telescope.lua
+++ .nvim/lua/plugins/telescope.lua
@@ -1,17 +1,53 @@
 return {
-  require('telescope').setup {
-    defaults = {
-      vimgrep_arguments = {
-        'rg',
-        '--no-ignore',
-        '-L',
-        '--hidden',
-        '--line-number',
-        '--column',
-        '--smart-case',
-        '--follow',         -- Follow symlinks
-        '--glob', '!.git/', -- (Optional) Ignore .git directory
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  config = function()
+    require('telescope').setup {
+
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--no-ignore',
+          '-L',
+          '--hidden',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--follow',         -- Follow symlinks
+          '--glob', '!.git/', -- (Optional) Ignore .git directory
+        },
       },
+      pickers = {
+        git_branches = {
+          theme = "dropdown",
+        }
+      }
+    }
+  end,
+  keys = {
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    {
+      "<leader>fp",
+      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+      desc = "Find Plugin File",
     },
-  }
+    {
+      "<leader>gS",
+      function() require("telescope.builtin").git_branches() end,
+      desc = "Find Git Branches",
+    }
+  },
+  -- change some options
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+    },
+  },
+
 }
